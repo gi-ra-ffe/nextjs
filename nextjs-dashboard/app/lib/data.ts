9c2af390c1e4d9befe8c6502ab9e545fd0edb8c2
Kie Takahashi <msxgrkt.mzgk@gmail.com>
2025-02-12 08:15:32 +0900

    fix: 第9章 まで

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -16,15 +16,7 @@ const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -42,15 +34,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    interface Invoice {
-      id: number;
-      customer_id: number;
-      amount: number;
-      status: string;
-      date: string;
-    }
-
-    const latestInvoices = data.map((invoice: Invoice) => ({
+    const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
